145db37d78cfec35b9b6bb636e055cb9cdcf450e
Andrew Aarestad <andrew@etal-andothers.com>
2014-01-10 21:57:32 -0600

    Initial Commit

---
+++ MKMapCamera-Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MKMapCamera-Demo
+//
+//  Created by Andrew on 1/10/14.
+//  Copyright (c) 2014 AA Engineering LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
